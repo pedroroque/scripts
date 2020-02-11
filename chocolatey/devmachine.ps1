a7f35905e4ded4051952d8902a9ab87d81746614
Pedro Roque <pedro.roque@outlook.com>
2020-02-11 20:30:54 -0300

    Bypass execution policy

--- chocolatey/devmachine.ps1
+++ chocolatey/devmachine.ps1
@@ -17,6 +17,7 @@ try {
     choco config get cachelocation
 } catch {
     Write-Output "Chocolatey parece não estar instalado. Tentando instalar"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
